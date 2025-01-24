b4500f915a07134907c3f45acb88e7e89a9ccc31
AbhiTope <abhijeettope@gmail.com>
2025-01-24 13:53:00 +0530

    moved logic to class

--- src/main.cpp
+++ src/main.cpp
@@ -5,91 +5,93 @@
 
 using namespace std;
 
-string getSystemDate() {
-  time_t now = time(0);
-  tm* ltm = localtime(&now);
+class YearGauge{
+        private:
+                string dateYearStart, dateCurrent;
+                double diff, ylPer, noOfSecondsInYear, days;
 
-  stringstream ss;
-  ss << setw(4) << setfill('0') << ltm->tm_year + 1900 << "-"; 
-  ss << setw(2) << setfill('0') << ltm->tm_mon + 1 << "-"; 
-  ss << setw(2) << setfill('0') << ltm->tm_mday; 
+                string getSystemDate() {
+                        time_t now = time(0);
+                        tm* ltm = localtime(&now);
+                        stringstream ss;
+                        ss << setw(4) << setfill('0') << ltm->tm_year + 1900 << "-";
+                        ss << setw(2) << setfill('0') << ltm->tm_mon + 1 << "-";
+                        ss << setw(2) << setfill('0') << ltm->tm_mday;
+                        return ss.str();
+                }
 
-  return ss.str();
-}
+                string yearStartDate(){
+                        time_t now = time(0);
+                        tm* ltm = localtime(&now);
+                        stringstream ss;
+                        ss << setw(4) << setfill('0') << ltm->tm_year + 1900 << "-";
+                        ss << setw(2) << setfill('0') << 1 << "-";
+                        ss << setw(2) << setfill('0') << 1;
+                        return ss.str();
+                }
 
-string yearStartDate(){
+                double getSecondsInCurrentYear(){
+                        time_t now = time(0);
+                        tm* ltm = localtime(&now);
+                        int year = ltm->tm_year + 1900;
+                        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                                return 31622400;
+                        else
+                                return 31536000;
+                }
+        public:
+                YearGauge(){
+                        dateYearStart = yearStartDate();
+                        dateCurrent = getSystemDate();
 
-  time_t now = time(0);
-  tm* ltm = localtime(&now);
+                        noOfSecondsInYear = getSecondsInCurrentYear();
 
-  stringstream ss;
-  ss << setw(4) << setfill('0') << ltm->tm_year + 1900 << "-"; 
-  ss << setw(2) << setfill('0') << 1 << "-"; 
-  ss << setw(2) << setfill('0') << 1; 
+                        struct tm tm1 = {};
+                        istringstream ss1(dateYearStart);
+                        ss1 >> get_time(&tm1, "%Y-%m-%d");
 
-  return ss.str();
-}
+                        struct tm tm2 = {};
+                        istringstream ss2(dateCurrent);
+                        ss2 >> get_time(&tm2, "%Y-%m-%d");
 
-double getSecondsInCurrentYear(){
-	//31536000 => seconds in a year
-	//31622400 => seconds in leap year
+                        time_t t1 = mktime(&tm1);
+                        time_t t2 = mktime(&tm2);
 
-	time_t now = time(0);
-	tm* ltm = localtime(&now);
-	int year = ltm->tm_year + 1900;
+                        diff = difftime(t2, t1);
+                        ylPer = (diff/noOfSecondsInYear) * 100;
+                }
 
-	if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-		return 31622400;
-	else
-		return 31536000;
-}
+                double getPassedPercent(){
+                        return ylPer;
+                }
+
+                double getPassedDays(){
+                        days = diff / (60 * 60 * 24);
+                        return days;
+                }
+
+};
 
 
 int main() {
 
-	string dateYearStart, dateCurrent;
+        YearGauge yg;
+        double ylPer = yg.getPassedPercent();
+        cout <<  "\033[1m" << 100 - ylPer << "%" << "\033[0m";
 
-	dateYearStart = yearStartDate();
-	dateCurrent = getSystemDate();
+        double count = 0;
 
-	//cout << "Enter the first date (YYYY-MM-DD): ";
-	//cin >> dateYearStart;
+        cout << " " << "[";
+        for(int i = 0; i <= 100; i++){
 
-	//cout << "Enter the second date (YYYY-MM-DD): ";
-	//cin >> dateCurrent;
+                if( i < ylPer)
+                        cout << "\033[31m#";
+                else
+                        cout << "\033[32m.";
+        }
+        cout << "\033[0m";
+        cout << "]";
 
-	struct tm tm1 = {};
-	istringstream ss1(dateYearStart);
-	ss1 >> get_time(&tm1, "%Y-%m-%d");
-
-	struct tm tm2 = {};
-	istringstream ss2(dateCurrent);
-	ss2 >> get_time(&tm2, "%Y-%m-%d");
-
-	// Convert tm structures to time_t
-	time_t t1 = mktime(&tm1);
-	time_t t2 = mktime(&tm2);
-
-	double diff = difftime(t2, t1);
-	double noOfSecondsInYear = getSecondsInCurrentYear();
-
-	double ylPer = (diff/noOfSecondsInYear) * 100; 
-
-	cout <<  "\033[1m" << 100 - ylPer << "%" << "\033[0m";
-
-	double days = diff / (60 * 60 * 24);
-	double count = 0;
-
-	cout << " " << "[";
-	for(int i = 0; i <= 100; i++){
-
-		if( i < ylPer)
-			cout << "\033[31m#";
-		else
-			cout << "\033[32m.";
-	}
-	cout << "\033[0m";
-	cout << "]";
-
-	return 0;
+        return 0;
 }
+
