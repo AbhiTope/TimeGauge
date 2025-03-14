be6820d28443ad2cd678835569a4dc8af881117e
AbhiTope <abhijeettope.at@gmail.com>
2025-03-14 16:09:41 +0530

    .

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+
+
+double days_in_month(int month){
+
+	if(month == 2){
+		time_t now = time(0); 
+		tm *ltm = localtime(&now);
+		int year = ltm->tm_year + 1900;
+		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+			return 29;
+		return 28;
+	}
+	else if((month % 2 == 0 && month < 7) || ( month % 2 != 0 && month > 7))
+		return 30;
+
+	return 31;
+}
+
 double getYL(){
 	time_t now = time(0);
 	time_t currentTime, yearStartTime;
@@ -34,44 +52,44 @@ double getYL(){
 
 }
 
-int days_in_month(int month){
-
-	if(month == 2){
-		time_t now = time(0); 
-		tm *ltm = localtime(&now);
-		int year = ltm->tm_year + 1900;
-		if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-			return 29;
-		return 28;
-	}
-	else if((month % 2 == 0 && month < 7) || ( month % 2 != 0 && month > 7))
-		return 30;
-
-	return 31;
+double getML(){
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	double month_days = days_in_month(ltm->tm_mon + 1);
+	return (ltm->tm_mday / month_days) * 100;
 }
 
-int main() {
+double getWL(){
+	time_t now = time(0);
+	tm *ltm = localtime(&now);
+	//return (double)ltm->tm_wday;
+	return (((double)ltm->tm_wday + 1)/7) * 100;
+}
 
-	double ylPer = getYL();
-        cout <<  "\033[1;31m" << ylPer << "%" << "\033[0m";
+void display(double per){
+
+	cout <<  "\033[1;31m" << per << "%" << "\033[0m";
 	for(int i = 0; i < 85; i++)
 		cout << " ";
-        cout <<  "\033[1;32m" << 100 - ylPer << "%" << "\033[0m";
+        cout <<  "\033[1;32m" << 100 - per << "%" << "\033[0m";
 	cout << endl;
-        //cout << " " << "[";
         for(int i = 0; i <= 100; i++){
 
-                if( i < ylPer)
+                if( i < per)
                         cout << "\033[101;31m ";
                 else
                         cout << "\033[102;32m ";
         }
         cout << "\033[0m";
-        //cout << "]";
-        //cout <<  "\033[1;32m" << 100 - ylPer << "%" << "\033[0m";
 	cout << endl;
 
+}
 
+int main() {
+
+	display(getYL());
+	display(getML());
+	display(getWL());
 
         return 0;
 }
